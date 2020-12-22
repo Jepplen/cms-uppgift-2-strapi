81780b13bbfd51e2b35de349a1ffda01645ca8e9
Jesper Bengtsson Ekwall <jeppen@live.com>
2020-12-22 11:30:57 +0100

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '99f31f42face4a501775dbf513a03d83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ba58ad4-d495-4e12-b420-bd7eab0813f4'
+};
